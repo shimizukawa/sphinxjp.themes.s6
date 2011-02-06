402fcc60f4bb84a43f38f58aed551b9931ccaab0
shimizukawa <devnull@localhost>
2011-02-06 17:33:37 +0900

    fix typo

--- example/conf.py
+++ example/conf.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 html_use_modindex = False
 html_use_index = False
-extensions = ['sphinxjp.themes.s6']
+extensions = ['sphinxjp.themecore']
 source_suffix = '.rst'
 master_doc = 'index'
 
